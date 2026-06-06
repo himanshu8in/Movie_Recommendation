6361c8b45a3f5704514ae69b70f466699f7a8a73
Himanshu Singh <himanshusinghrajput140404@gmail.com>
2026-06-06 10:42:32 +0530

    Initial Movie Recommendation System

---
+++ debug_app.py
@@ -0,0 +1,28 @@
+print("Step 1")
+import streamlit as st
+
+print("Step 2")
+import pandas as pd
+
+print("Step 3")
+import pickle
+
+print("Step 4")
+from src.preprocess import load_data
+
+print("Step 5")
+from src.collaborative import recommend_movies
+
+print("Step 6")
+ratings, movies, users = load_data("MovieLens-Recommender/data")
+
+print("Ratings:", ratings.shape)
+print("Movies:", movies.shape)
+print("Users:", users.shape)
+
+print("Step 7")
+with open("MovieLens-Recommender/models/svd_model.pkl", "rb") as f:
+    model = pickle.load(f)
+
+print("Step 8")
+print("Everything loaded successfully")
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+MoviesLens-Recommender
